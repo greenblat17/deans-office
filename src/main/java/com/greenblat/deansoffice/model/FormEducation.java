61370ceeea342744f3fa2213e4286cfc6d2c6c82
Alex Zhuravlev <leftemail17@mail.ru>
2023-12-12 18:51:51 +0300

    add name constructor

--- src/main/java/com/greenblat/deansoffice/model/FormEducation.java
+++ src/main/java/com/greenblat/deansoffice/model/FormEducation.java
@@ -1,5 +1,10 @@
 package com.greenblat.deansoffice.model;
 
 public enum FormEducation {
-    DAYTIME, EVENING, CORRESPONDENCE
+    DAYTIME("daytime"),
+    EVENING("evening"),
+    CORRESPONDENCE("correspondence");
+
+    FormEducation(String name) {
+    }
 }
