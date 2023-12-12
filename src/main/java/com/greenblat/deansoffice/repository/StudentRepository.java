8eb7f0c418546838b02208dd39da284440e897f6
Alex Zhuravlev <leftemail17@mail.ru>
2023-12-12 17:24:10 +0300

    feat: add create and update queries for student

--- src/main/java/com/greenblat/deansoffice/repository/StudentRepository.java
+++ src/main/java/com/greenblat/deansoffice/repository/StudentRepository.java
@@ -1,9 +1,14 @@
 package com.greenblat.deansoffice.repository;
 
 import com.greenblat.deansoffice.model.FormEducation;
+import com.greenblat.deansoffice.model.Student;
 
 public interface StudentRepository {
 
     Integer countByFormEducation(FormEducation formEducation);
+
+    void insertStudent(Student student);
+
+    void updateStudent(Student student);
 
 }
--- src/main/java/com/greenblat/deansoffice/repository/impl/StudentRepositoryImpl.java
+++ src/main/java/com/greenblat/deansoffice/repository/impl/StudentRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.greenblat.deansoffice.repository.impl;
 
 import com.greenblat.deansoffice.model.FormEducation;
+import com.greenblat.deansoffice.model.Student;
 import com.greenblat.deansoffice.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -20,6 +21,44 @@ public class StudentRepositoryImpl implements StudentRepository {
                 WHERE form_education = ?
                 """;
         return jdbcTemplate.queryForObject(sql, Integer.class, formEducation.name());
+    }
+
+    @Override
+    public void insertStudent(Student student) {
+        var sql = """
+                INSERT INTO student (last_name, first_name, surname, university_year_admission, form_education, group_number)
+                VALUES (?, ?, ?, ?, ?, ?)
+                """;
+        jdbcTemplate.update(
+                sql,
+                student.getLastName(),
+                student.getFirstName(),
+                student.getSurname(),
+                student.getUniversityYearAdmission(),
+                student.getFormEducation(),
+                student.getGroupNumber()
+        );
+    }
+
+    @Override
+    public void updateStudent(Student student) {
+        var sql = """
+                UPDATE student
+                SET 
+                    (last_name, first_name, surname, university_year_admission, form_education, group_number)
+                        = (?, ?, ?, ?, ?, ?)
+                WHERE id = ?
+                """;
+        jdbcTemplate.update(
+                sql,
+                student.getLastName(),
+                student.getFirstName(),
+                student.getSurname(),
+                student.getUniversityYearAdmission(),
+                student.getFormEducation(),
+                student.getGroupNumber(),
+                student.getId()
+        );
     }
 
 
