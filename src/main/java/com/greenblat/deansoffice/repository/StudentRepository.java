5bcaac7cd98de4e36eb3220f56c021eafa9d4e0e
Alex Zhuravlev <leftemail17@mail.ru>
2023-12-13 12:34:05 +0300

    add find by id method for student

--- src/main/java/com/greenblat/deansoffice/repository/StudentRepository.java
+++ src/main/java/com/greenblat/deansoffice/repository/StudentRepository.java
@@ -3,6 +3,8 @@ package com.greenblat.deansoffice.repository;
 import com.greenblat.deansoffice.model.FormEducation;
 import com.greenblat.deansoffice.model.Student;
 
+import java.util.Optional;
+
 public interface StudentRepository {
 
     Integer countByFormEducation(FormEducation formEducation);
@@ -10,5 +12,7 @@ public interface StudentRepository {
     void insertStudent(Student student);
 
     void updateStudent(Student student);
+
+    Optional<Student> findById(Long id);
 
 }
--- src/main/java/com/greenblat/deansoffice/repository/impl/StudentRepositoryImpl.java
+++ src/main/java/com/greenblat/deansoffice/repository/impl/StudentRepositoryImpl.java
@@ -3,15 +3,19 @@ package com.greenblat.deansoffice.repository.impl;
 import com.greenblat.deansoffice.model.FormEducation;
 import com.greenblat.deansoffice.model.Student;
 import com.greenblat.deansoffice.repository.StudentRepository;
+import com.greenblat.deansoffice.repository.mapper.StudentRowMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class StudentRepositoryImpl implements StudentRepository {
 
     private final JdbcTemplate jdbcTemplate;
+    private final StudentRowMapper studentRowMapper;
 
     @Override
     public Integer countByFormEducation(FormEducation formEducation) {
@@ -59,6 +63,18 @@ public class StudentRepositoryImpl implements StudentRepository {
                 student.getGroupNumber(),
                 student.getId()
         );
+    }
+
+    @Override
+    public Optional<Student> findById(Long id) {
+        var sql = """
+                SELECT id, last_name, first_name, surname, university_year_admission, form_education, group_number
+                FROM student
+                WHERE id = ?
+                """;
+        return jdbcTemplate.query(sql, studentRowMapper, id)
+                .stream()
+                .findFirst();
     }
 
 
