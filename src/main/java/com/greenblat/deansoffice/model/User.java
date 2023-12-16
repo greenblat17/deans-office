68bb7c84ebea59771d97ff0ebb573b44e660ff42
Alex Zhuravlev <leftemail17@mail.ru>
2023-12-16 17:13:31 +0300

    add user entity

---
+++ src/main/java/com/greenblat/deansoffice/model/User.java
@@ -0,0 +1,25 @@
+package com.greenblat.deansoffice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+}
